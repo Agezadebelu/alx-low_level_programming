b11efb07ce188b823764fad7bf9b118cac97687d
Agezadebelu <lateradeb2019@gmail.com>
2023-06-29 05:18:57 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,8 +5,7 @@
  *@dest: pointer to destination string.
  *@src: pointer to source string.
  *@n: number of bytes to be concatenated.
- *@j: is a counter for  n bytes of src to be concatenated
- *@i: length of destination string
+ *
  *Return: pointer to destination string.
  */
 char *_strncat(char *dest, char *src, int n)
