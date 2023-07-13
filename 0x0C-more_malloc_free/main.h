0ec98e82b02f240a01837407f49eb3a1e6143328
Agezadebelu <lateradeb2019@gmail.com>
2023-07-13 12:26:15 -0700

    0x0C-more_malloc_free

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -13,17 +13,5 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-void errors(void);
-int _strlen(char *s);
-int is_digit(char *s);
-int find_len(char *str);
-char *create_xarray(int size);
-char *iterate_zeroes(char *str);
-void get_prod(char *prod, char *mult, int digit, int zeroes);
-void add_nums(char *final_prod, char *next_prod, int next_len);
-int len(char *str);
-void _is_zero(char *argv[]);
-char *_initialize_array(char *ar, int lar);
-int _checknum(char *argv[], int n);
 
 #endif
