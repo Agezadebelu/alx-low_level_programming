abdc222dd4592a66100daf3e43759a987ee4796d
Agezadebelu <lateradeb2019@gmail.com>
2023-11-17 12:12:05 -0800

    updated

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/libhack.so https://github.com/Agezadebelu/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libhack.so
+wget -q -O /tmp/libhack.so https://github.com/Agezadebelu/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libhack.so
 export LD_PRELOAD=/tmp/libhack.so
