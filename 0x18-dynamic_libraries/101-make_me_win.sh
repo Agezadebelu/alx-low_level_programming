426b5c00a1d2e2ec00e21265e93dbc80d958664b
Agezadebelu <lateradeb2019@gmail.com>
2023-11-17 13:10:09 -0800

    updated

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P ..  https://raw.githubusercontent.com/Agezadebelu/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD="$PWD/../nrandom.so"
+wget -P .. https://raw.githubusercontent.com/Agezadebelu/alx-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD="tmp/../libgiga.so"
