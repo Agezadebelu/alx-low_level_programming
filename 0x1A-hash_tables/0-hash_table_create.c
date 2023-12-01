e6d87373df0b711b3e3c922b418862f340aa9e0d
Agezadebelu <lateradeb2019@gmail.com>
2023-11-30 21:55:20 -0800

    updated

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,13 +12,16 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_node_t **array;
 	unsigned long int i;
 
-	table = malloc(sizeof(hash_table_t));
+	table = (hash_table_t *) malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
 
-	array = malloc(sizeof(hash_node_t *) * size);
+	array = (hash_node_t **) malloc(sizeof(hash_node_t *) * size);
 	if (array == NULL)
+	{
+		free(array);
 		return (NULL);
+	}
 
 	for (i = 0; i < size; i++)
 		array[i] = NULL;
