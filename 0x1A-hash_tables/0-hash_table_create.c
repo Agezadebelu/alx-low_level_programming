b141c045cffb73b004bd63ffcc0424c740ab4302
Agezadebelu <lateradeb2019@gmail.com>
2023-11-30 21:38:13 -0800

    updated

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,7 +8,7 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table;
+	hash_table_t *table = NULL;
 	hash_node_t **array;
 	unsigned long int i;
 
@@ -23,7 +23,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	for (i = 0; i < size; i++)
 		array[i] = NULL;
 
-	table->array = array;
+	/*table->array = array;*/
 	table->size = size;
 
 	return (table);
